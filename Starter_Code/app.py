4084513f37548e1f0503c8352354091097450a95
Marshal Rittenger <mrrittenger@gmail.com>
2024-02-27 18:13:07 -0800

    FINAL PROJECT COMPLETE

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -40,7 +40,7 @@ app = Flask(__name__)
 most_recent_date = dt.date(2017, 8, 23)
 year_ago = most_recent_date - dt.timedelta(days=365)
 
-
+# Route for Landing Page
 @app.route("/")
 def welcome():
     return (
@@ -52,15 +52,18 @@ def welcome():
         f"/api/v1.0/<start>/<end>"
     )
 
+
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     
+    # Setting variables and querying for a year of precipitation data
     date = Measurement.date
     year_of_rain_data = session.query(date, Measurement.prcp)\
     .filter(date >= year_ago, date <= most_recent_date).all()
 
     session.close()
 
+    # Creating a list and dictionaries to put the precipitation data amd returning it
     precipitation_levels = []
     for date, prcp in year_of_rain_data:
         precipitation_dict = {}
@@ -70,13 +73,17 @@ def precipitation():
 
     return jsonify(precipitation_levels)
 
+
 @app.route("/api/v1.0/stations")
 def stations():
+
+    # Setting variables and querying to find the unique stations
     station_column = Station.station
     active_stations = session.query(station_column)
 
     session.close()
 
+    # Inserting the results into a list and returning it
     station_list = []
     for station in active_stations:
         station_list.append(station)
@@ -85,25 +92,29 @@ def stations():
     return jsonify(station_list)
 
 
-
 @app.route("/api/v1.0/tobs")
 def temperature():
+
+    # Setting Variables
     station_row = Measurement.station
     temperature = Measurement.tobs
     date = Measurement.date
 
+    # Querying to find the most active stations and isolating the single most active station
     active_stations = session.query(station_row, func.count(station_row))\
     .group_by(station_row)\
     .order_by(func.count(station_row).desc()).all()
     
     most_active_station = active_stations[0][0]
 
+    # Querying a year of temperature data from that most active station
     year_of_temp_data = session.query(date, temperature)\
     .filter(station_row == most_active_station)\
     .filter(date >= year_ago, date <= most_recent_date).all()
 
     session.close()
 
+    # Inserting the data into the list and returning the data
     temp_list = []
     for temp in year_of_temp_data:
         temp_list.append(temp)
@@ -111,8 +122,11 @@ def temperature():
     temp_list = list(np.ravel(temp_list))
     return jsonify(temp_list)
 
+# Creating a route for temperature ranges from a start date to the end of the data
 @app.route("/api/v1.0/<start>")
 def temperature_range_1(start):
+
+    # Setting Variables and querying the temperatures starting at the input date in the url 
     date = Measurement.date
     temperature = Measurement.tobs
     
@@ -121,6 +135,7 @@ def temperature_range_1(start):
 
     session.close()
 
+    # Inserting the data into the list and returning the data
     temp_range_list = []
     for temp in range_of_temp_data:
         temp_range_list.append(temp)
@@ -130,6 +145,9 @@ def temperature_range_1(start):
 
 @app.route("/api/v1.0/<start>/<end>")
 def temperature_range_2(start, end):
+
+    # Setting Variables and querying the temperatures starting at the input date in the url 
+    # AND using the end date in the url as well
     date = Measurement.date
     temperature = Measurement.tobs
     
@@ -138,6 +156,7 @@ def temperature_range_2(start, end):
 
     session.close()
 
+    # Inserting the data into the list and returning the data
     temp_range_list2 = []
     for temp in range_of_temp_data:
         temp_range_list2.append(temp)
